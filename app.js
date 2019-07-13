45cc5de29228f8174ee11a339b15fc3f0ea5cfb7
Kaitian Xie <xkaitian@gmail.com>
2019-07-13 14:18:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const config = require('./config');
+const messagesRouter = require('./routes/messages');
+
+const app = express();
+
+// connect to mongodb
+mongoose
+  .connect(config.mongo.uri, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err.message));
+
+// view engine setup
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/messages', messagesRouter);
+app.use(express.static('client/build'));
+if (process.ENV.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.ENV.port || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
